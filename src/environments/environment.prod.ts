fe464a4500d82e7a735d1b1dd22045450cef90e3
Fily12 <filythiaw0@gmail.com>
2026-06-29 16:59:52 +0000

    ajout configuration environnements et proxy CORS

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  walletApiUrl: 'http://localhost:8080',
+  paymentApiUrl: 'http://localhost:8081'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  walletApiUrl: 'http://localhost:8080',
+  paymentApiUrl: 'http://localhost:8081'
+};
